1dca8086f0812a9d7c259b93b7e6a75875af7969
Zhenbang Chen <52593219+PeanutsLOL@users.noreply.github.com>
2026-02-13 19:43:47 +0800

    Delete apply_bleed2.ps1

--- apply_bleed2.ps1
+++
@@ -1,4 +0,0 @@
-$files = Get-ChildItem "Content/Buffs" -Filter "*.png" -File -Recurse | Where-Object { $_.FullName -notlike "*\Backup\*" }
-foreach ($f in $files) {
-    magick "$($f.FullName)" -set option:distort:viewport "%[fx:w+4]x%[fx:h+4]-2-2" -virtual-pixel Edge -filter point -distort SRT 0 +repage "$($f.FullName)"
-}
